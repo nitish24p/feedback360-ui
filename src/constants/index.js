9b1368e0132bde9c47c080814aa2f3d22056f234
NitishPhanse <nitsums@gmail.com>
2019-09-03 02:36:01 +0530

    ssl

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 const BASE =
   process.env.NODE_ENV === 'production'
-    ? 'http://feedback360api.now.sh'
+    ? 'https://feedback360api.now.sh'
     : 'http://localhost:5000';
 
 export const Urls = {
